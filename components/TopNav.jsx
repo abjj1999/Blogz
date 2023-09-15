f5217ebe02bdf304f6874f75b6dc7491661524b6
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-09-15 12:59:11 -0500

    created nav

---
+++ components/TopNav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'next/link'
+
+
+const TopNav = () => {
+  return (
+    <nav className="
+        nav shadow p-2 justify-content-between mb-3 font-weight-bold 
+    ">
+        <Link className='nav-link text-dark  ' href="/">
+            BLOGS
+        </Link>
+
+        <div className="d-flex ">
+            <Link className='nav-link text-dark' href="/login">
+                LogIn
+            </Link>
+            <Link className='nav-link text-dark' href="/register">
+                Register
+            </Link>
+        </div>
+    </nav>
+  )
+}
+
+export default TopNav
